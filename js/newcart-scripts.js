24015637428d8efa3d2ab589890855dce4a7a14c
cassandra culkins and will murphy <cassy.culkins@gmail.com>
2016-02-03 10:32:21 -0800

    working module for new user info

--- js/newcart-scripts.js
+++ js/newcart-scripts.js
@@ -44,21 +44,21 @@ $(document).ready(function() {
                                 '<h2><span class="glyphicon glyphicon-cutlery"></span> ' +
                                 '<span id="cart-name">' + cartName + '</span></h2> <hr>' + '<div class="cartImage">' +
                                 '<img class="img-responsive"  src=' + foodCartPic + '></div>' + "<hr>" + '<p>Owner:' + ownerName + '</p>' +
-                                // 
-                                // '<img type="button" class="menuPic" href="' + menuLink + 'data-toggle="modal" data-target="#' + ownerName + '>' +
-                                // '<div class="modal fade" id="' + ownerName + '" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">' +
-                                //   '<div class="modal-dialog" role="document">' +
-                                //     '<div class="modal-content">' +
-                                //       '<div class="modal-header">' +
-                                //         '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' + '<span aria-hidden="true">&times;</span></button>' +
-                                //         '<h4 class="modal-title" id="myModalLabel">Food Cart Menu</h4>' +
-                                //       '</div>' +
-                                //       '<div class="modal-body">' +
-                                //         '<img class="modalPic" src="' + menuLink + '">' +
-                                //       '</div>' +
-                                //     '</div>' +
-                                //   '</div>' +
-                                // '</div>' +
+
+                                '<a type="button" class="menuPic" href="' + menuLink + '" data-toggle="modal" data-target="#' + ownerName + '">' + '<img class="menuPic" src="' + menuLink + '">' + '</a>' +
+                                '<div class="modal fade" id="' + ownerName + '" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">' +
+                                  '<div class="modal-dialog" role="document">' +
+                                    '<div class="modal-content">' +
+                                      '<div class="modal-header">' +
+                                        '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' + '<span aria-hidden="true">&times;</span></button>' +
+                                        '<h4 class="modal-title" id="myModalLabel">Food Cart Menu</h4>' +
+                                      '</div>' +
+                                      '<div class="modal-body">' +
+                                        '<img class="modalPic" src="' + menuLink + '">' +
+                                      '</div>' +
+                                    '</div>' +
+                                  '</div>' +
+                                '</div>' +
 
 
                                  '<p>Link to location:' + locationLink + '</p>');
